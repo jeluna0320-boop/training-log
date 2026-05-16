135f6cc9da9cc0890d0b88887eb7bc3414257c79
jeluna0320-boop <jeluna0320@gmail.com>
2026-05-16 13:13:25 -0500

    Update sw.js
    bug

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Service worker for Training Log PWA
 // Caches the app shell so it works offline after first load
 
-const CACHE_NAME = 'training-log-v4';
+const CACHE_NAME = 'training-log-v5';
 const ASSETS = [
   './',
   './index.html',
