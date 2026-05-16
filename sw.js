e7e9aa15485495049caeae94724973bde2c49685
jeluna0320-boop <jeluna0320@gmail.com>
2026-05-16 13:00:11 -0500

    Update sw.js
    weight

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Service worker for Training Log PWA
 // Caches the app shell so it works offline after first load
 
-const CACHE_NAME = 'training-log-v3';
+const CACHE_NAME = 'training-log-v4';
 const ASSETS = [
   './',
   './index.html',
